0c9772e07efa145e9692850303df42e6cfafe63f
Kostosmfr <kostosmfr@gmail.com>
2017-06-26 23:01:27 +0300

    test

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,13 @@
+/**
+ * Created by K_PC-S on 21.06.2017.
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        Name n = new Name("Your Name");
+
+        System.out.println("Hello " + n.getName());
+
+    }
+}
---
+++ src/main/java/Name.java
@@ -0,0 +1,23 @@
+/**
+ * Created by K_PC-S on 21.06.2017.
+ */
+public class Name {
+
+    String Name;
+
+    public Name(String Name)
+    {
+        setName(Name);
+
+    }
+
+    void setName(String Name) {
+        this.Name = Name;
+    }
+
+    String getName() {
+        return Name;
+    }
+
+
+}
